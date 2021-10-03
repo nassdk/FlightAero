73b3961dd29e513ffb25b2c7c311061341d66de6
Sadyk Nasrullaev <nassdk17@gmail.com>
2021-10-03 21:26:52 +0300

    Added ui setup base method

--- core-common/src/main/java/com/nassdk/corecommon/base/BaseFragment.kt
+++ core-common/src/main/java/com/nassdk/corecommon/base/BaseFragment.kt
@@ -2,6 +2,7 @@ package com.nassdk.corecommon.base
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.nassdk.corecommon.enums.TransitionType
@@ -12,6 +13,7 @@ abstract class BaseFragment(@LayoutRes layoutResId: Int) : Fragment(layoutResId)
     open val transitionType: TransitionType = TransitionType.HORIZONTAL
 
     protected abstract fun setupInjection()
+    protected open fun setupUi() = Unit
 
     override fun onAttach(context: Context) {
         setupInjection()
@@ -21,6 +23,11 @@ abstract class BaseFragment(@LayoutRes layoutResId: Int) : Fragment(layoutResId)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupInjection()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUi()
     }
 
     override fun onDestroy() {
