bd05309a36d4cb8649a70f7fe6b4565e9cdbbb95
Sadyk Nasrullaev <nassdk17@gmail.com>
2021-12-17 17:12:15 +0300

    Added error handler.

---
+++ core-common/src/main/java/com/nassdk/corecommon/error/CoreErrorHandler.kt
@@ -0,0 +1,8 @@
+package com.nassdk.corecommon.error
+
+object CoreErrorHandler {
+    //Stub
+    fun mapException(throwable: Throwable): ErrorWrapper {
+        return ErrorWrapper.Unknown
+    }
+}
--- core-common/src/main/java/com/nassdk/corecommon/error/CoroutinesErrorHandler.kt
+++ core-common/src/main/java/com/nassdk/corecommon/error/CoroutinesErrorHandler.kt
@@ -4,7 +4,12 @@ import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineExceptionHandler
 
-internal class CoroutinesErrorHandler : AbstractCoroutineContextElement(CoroutineExceptionHandler),
-    CoroutineExceptionHandler {
-    override fun handleException(context: CoroutineContext, exception: Throwable) = Unit
+internal class CoroutinesErrorHandler(
+    private val exceptionHandler: ((ErrorWrapper) -> Unit)? = null,
+) : AbstractCoroutineContextElement(CoroutineExceptionHandler), CoroutineExceptionHandler {
+
+    override fun handleException(context: CoroutineContext, exception: Throwable) {
+        val mappedException = CoreErrorHandler.mapException(throwable = exception)
+        exceptionHandler?.invoke(mappedException)
+    }
 }
